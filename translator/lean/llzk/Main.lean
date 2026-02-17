406ef48244e160c89c622c7e3574501c83031ef0
Samir Genaim <genaim@gmail.com>
2026-02-17 15:44:55 +0100

    refactor: update dependencies and improve pretty-printing functionality

--- translator/lean/llzk/Main.lean
+++ translator/lean/llzk/Main.lean
@@ -1,24 +1,52 @@
 import Llzk.Language.Core.Syntax.AST
 import Llzk.Language.Core.Syntax.Parser
 import Llzk.Basic
+import Cli
 
 open Llzk.Language.Core.Syntax.AST
 open Llzk.Language.Core.Syntax.Parser
 
+open Cli
+
+
+
+def prettyPrinting (inFile : String) (outStream : IO.FS.Stream) : IO Unit := do
+     IO.println s!"Parsing program {inFile}..."
+     let initialState ← ParserM.fromFile inFile
+     let (prog,_) ← StateT.run (@parseProg F11 []) initialState
+     IO.println s!"Printing program ..."
+     @printProg F11 outStream prog.reverse
+     outStream.flush
+
+
+
+def runLlzkCmd (p : Parsed) : IO UInt32 := do
+  let input : String := p.positionalArg! "input" |>.as! String
+  let outStream ← if p.hasFlag "output" then
+    let output : String := p.flag! "output" |>.as! String
+    let h ← IO.FS.Handle.mk output IO.FS.Mode.write
+    pure (IO.FS.Stream.ofHandle h)
+  else
+    IO.getStdout
+  prettyPrinting input outStream
+  return 0
+
+
+
+def llzkCmd : Cmd := `[Cli|
+  llzkCmd VIA runLlzkCmd; ["0.0.1"]
+  "Translator for Core Llzk programs."
+  FLAGS:
+    pp, prettyprinting;         "Parse and pretty-print the input program."
+    o, output : String;         "The output file. If not provided, stdout is used."
+  ARGS:
+    input : String;      "The input program"
+]
+
 
 /-- Main entry point for symbolic execution from command line -/
 def main (args : List String) : IO UInt32 := do
-  match args with
-  | [inFile,outFile] =>
-    IO.FS.withFile outFile IO.FS.Mode.write fun outHandle => do
-      let outStream := IO.FS.Stream.ofHandle outHandle
-      IO.println s!"Parsing program {inFile}..."
-      let initialState ← ParserM.fromFile inFile
-      let (prog,_) ← StateT.run (@parseProg F11 []) initialState
-      IO.println s!"Printing program into {outFile}..."
-      @printProg F11 outStream prog.reverse
-      outStream.flush
-      return 0
-  | _ =>
-    IO.println "Usage: program <input_file> <output_file>"
-    return 1
+  llzkCmd.validate args
+
+
+-- #eval main <| "-o output/out.txt test_progs/syntax/simple.txt".splitOn " "
