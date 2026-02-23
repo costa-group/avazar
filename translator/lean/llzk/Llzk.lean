21f8c8da77011b60e9e4a2af1c8118434c3a07af
Samir Genaim <genaim@gmail.com>
2026-02-23 12:05:48 +0100

    refactoring

--- translator/lean/llzk/Llzk.lean
+++ translator/lean/llzk/Llzk.lean
@@ -1,4 +1,14 @@
 import Llzk.Basic
 import Llzk.Language.Core.Syntax.AST
+import Llzk.Language.Core.Syntax.Lexer
 import Llzk.Language.Core.Syntax.Parser
+import Llzk.Language.Core.Syntax.Printer
+
+import Llzk.Language.Core.Analysis.Liveness
+import Llzk.Language.Core.Semantics.Basic
 import Llzk.Language.Core.Semantics.BigStep
+import Llzk.Language.Core.Semantics.SmallStep
+
+import Llzk.FFConstraints.Basic
+import Llzk.FFConstraints.Satisfiability
+import Llzk.FFConstraints.SMT
--- translator/lean/llzk/Llzk/Language/Core/Semantics/SmallStep.lean
+++ translator/lean/llzk/Llzk/Language/Core/Semantics/SmallStep.lean
@@ -1,10 +1,11 @@
 import Llzk.Basic
-import Llzk.Language.Core.Syntax
+import Llzk.Language.Core.Syntax.AST
 import Llzk.Language.Core.Semantics.Basic
 
 namespace Llzk.Language.Core.Semantics.SmallStep
 
-open Llzk.Language.Core
+open Llzk.Language.Core.Syntax.AST
+open Llzk.Language.Core.Semantics.Basic
 
 
 
