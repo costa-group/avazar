23202d83687fa663a3170703cb4caf1b5c6c72e1
Samir Genaim <genaim@gmail.com>
2026-02-23 11:54:43 +0100

    refactored FFConstraints into several modules, and moved it outside of the symbolic execution module

---
+++ translator/lean/llzk/Llzk/FFConstraints/Basic.lean
@@ -0,0 +1,151 @@
+import Llzk.Basic
+import Llzk.Language.Core.Syntax.AST
+
+/- This module provides a syntax for SMT formulas over finite fields, their satisfiability,
+   and functions to print them in SMT-LIB2 format.
+-/
+namespace Llzk.FFConstraints.Basic
+
+open Llzk.Language.Core.Syntax.AST
+
+/- Metadata for FF variables -/
+structure FFVarMetaData where
+  orig_name : String
+  src_info : SrcInfo
+  deriving Repr, BEq, Inhabited
+
+structure BoolVarMetaData where
+  orig_name : String
+  src_info : SrcInfo
+  deriving Repr, BEq, Inhabited
+
+/- A finite field variable -/
+structure FFVar where
+  id : ℕ
+  meta_data: FFVarMetaData
+  deriving Repr, Inhabited
+
+/- A boolean variable -/
+structure BoolVar where
+  id : ℕ
+  meta_data: BoolVarMetaData
+  deriving Repr, Inhabited
+
+/- A variable, which can be either a finite field variable or a boolean variable -/
+abbrev Var := FFVar ⊕ BoolVar
+
+/-  Equality (BEq) of FFVar -/
+instance : BEq FFVar where
+  beq a b := a.id == b.id && a.meta_data.orig_name == b.meta_data.orig_name
+
+/-  Equality (BEq) of BoolVar -/
+instance : BEq BoolVar where
+  beq a b := a.id == b.id && a.meta_data.orig_name == b.meta_data.orig_name
+
+/-  Hashing (Hashable) of FFVar. Needed if we use this in a HashMap or HashSet -/
+instance : Hashable FFVar where
+  hash a := mixHash (hash a.id) (hash a.meta_data.orig_name)
+
+/-  Hashing (Hashable) of BoolVar. Needed if we use this in a HashMap or HashSet -/
+instance : Hashable BoolVar where
+  hash a := mixHash (hash a.id) (hash a.meta_data.orig_name)
+
+/-  Ordering (Ord) of FFVar. Needed if we use ordered sets -/
+instance : Ord FFVar where
+  compare a b :=
+    match compare a.meta_data.orig_name b.meta_data.orig_name with
+    | .eq => compare a.id b.id -- Names equal? Check IDs
+    | ord => ord               -- Names different? Return that order
+
+/-  Ordering (Ord) of BoolVar. Needed if we use ordered sets -/
+instance : Ord BoolVar where
+  compare a b :=
+    match compare a.meta_data.orig_name b.meta_data.orig_name with
+    | .eq => compare a.id b.id -- Names equal? Check IDs
+    | ord => ord               -- Names different? Return that order
+
+
+/- Term is a polynomial expression over finite fields -/
+inductive FFTerm (c : ZKConfig) where
+  | const : FF c →  FFTerm c
+  | var   : FFVar → FFTerm c
+  | add   : FFTerm c → FFTerm c → FFTerm c
+  | sub   : FFTerm c → FFTerm c → FFTerm c
+  | mul   : FFTerm c → FFTerm c → FFTerm c
+  deriving Repr, BEq, Inhabited
+
+/- A formula is a boolean formula with P(x)=0 as atoms.  -/
+inductive FFFormula (c : ZKConfig) where
+  | true   : FFFormula c
+  | false  : FFFormula c
+  | bool   : BoolVar → FFFormula c        -- A boolean variable
+  | eqZero : FFTerm c → FFFormula c       -- P(x) = 0
+  | and    : FFFormula c → FFFormula c → FFFormula c -- and
+  | or     : FFFormula c → FFFormula c → FFFormula c -- or
+  | not    : FFFormula c → FFFormula c -- negation
+  | ite    : FFFormula c → FFFormula c → FFFormula c → FFFormula c  -- if-then-else
+  | imply  : FFFormula c → FFFormula c → FFFormula c  -- implication
+  | iff    : FFFormula c → FFFormula c → FFFormula c  -- if and only if
+  | call   : String → List Var → FFFormula c  -- macro call
+  deriving Repr, BEq, Inhabited
+
+/- Trivial definition for size of formula, to be used for proving termination.
+   Tried to use the default sizeOf but failed to unfold at some point.
+   Revisit this later. -/
+def sizeOfFormula {c : ZKConfig} : FFFormula c → Nat
+  | .true | .false => 1
+  | .bool _ => 1
+  | .eqZero _ => 1
+  | .and a b | .or a b | .imply a b | .iff a b => 1 + sizeOfFormula a + sizeOfFormula b
+  | .not a => 1 + sizeOfFormula a
+  | .ite c t e => 1 + sizeOfFormula c + sizeOfFormula t + sizeOfFormula e
+  | .call _ _ => 1
+
+/- A macro is a named formula with parameters -/
+structure FFMacro (c : ZKConfig) where
+  name : String
+  params : List Var
+  body : FFFormula c
+  deriving Repr, BEq, Inhabited
+
+/- A constraint system consists of a list of macros and the name of the main macro -/
+structure FFConstraintSystem (c : ZKConfig) where
+  macros : List (FFMacro c)
+  main : String
+  deriving Repr, BEq, Inhabited
+
+/-- Fetch a macro by name from a list of macros. Throws an error if
+    the macro is not found. -/
+def fetchMacro {c : ZKConfig}
+    (ms : List (FFMacro c)) (name : String) : Except String (FFMacro c × List (FFMacro c)) :=
+  match ms with
+  | [] => Except.error s!"Macro {name} not found"
+  | m :: rest =>
+      if m.name == name then Except.ok (m, rest)
+      else fetchMacro rest name
+
+/- fetchMacro returns a smaller list of macros -/
+theorem fetchMacroLT {c : ZKConfig} (ms ms' : List (FFMacro c)) (name : String) (m : FFMacro c) :
+  fetchMacro ms name = Except.ok (m, ms') → ms'.length < ms.length := by
+  cases ms with
+  | nil => simp [fetchMacro]
+  | cons head tail =>
+      simp only [fetchMacro]
+      by_cases h : head.name = name
+      · simp only [h, BEq.rfl, ↓reduceIte, Except.ok.injEq, Prod.mk.injEq, List.length_cons,
+        Order.lt_add_one_iff, and_imp] at *
+        intro h1 h2
+        rw [h2]
+      · simp only [h, not_false_eq_true, beq_iff_eq, ↓reduceIte, List.length_cons,
+        Order.lt_add_one_iff] at *
+        intro h1
+        have h2 := fetchMacroLT tail ms' name m h1
+        grind
+
+/- The main formula of a constraint system is a call to the main macro -/
+def mainFormula {c : ZKConfig}
+  (sys : FFConstraintSystem c) : Except String (FFFormula c × List Var) := do
+  let (m,_) ← fetchMacro sys.macros sys.main
+  return (FFFormula.call m.name m.params, m.params)
+
+end Llzk.FFConstraints.Basic
---
+++ translator/lean/llzk/Llzk/FFConstraints/Satisfiability.lean
@@ -0,0 +1,98 @@
+import Llzk.Basic
+import Llzk.FFConstraints.Basic
+
+namespace Llzk.FFConstraints.Satisfiability
+
+open Llzk.FFConstraints.Basic
+open Llzk.Language.Core.Syntax.AST
+
+/- An assignment maps variables to values. There are two types of
+   variables, finite field variables and boolean variables, so we have
+   two maps -/
+structure Assignment (c : ZKConfig) where
+  ff : ℕ → FF c
+  bool : ℕ → Bool
+  deriving Inhabited
+
+/- Renaming of variables for macro calls -/
+def newAssignment' {c : ZKConfig}
+   (assign : Assignment c)
+   (orginNames newNames : List Var)
+   (ffMap : ℕ → FF c)
+   (boolMap : ℕ → Bool) : Except String (Assignment c) :=
+  match orginNames, newNames with
+  | [], [] => Except.ok { ff := ffMap, bool := boolMap }
+  | (.inl org) :: orgs, (.inl new) :: news =>
+    let ffMap' : ℕ → FF c := fun id => if id == new.id then assign.ff org.id else ffMap id
+    newAssignment' assign orgs news ffMap' boolMap
+  | (.inr org) :: orgs, (.inr new) :: news =>
+    let boolMap' : ℕ → Bool := fun id => if id == new.id then assign.bool org.id else boolMap id
+    newAssignment' assign orgs news ffMap boolMap'
+  | _, _ => Except.error "Mismatched variable lists"
+
+def newAssignment {c : ZKConfig}
+   (assign : Assignment c)
+   (orginNames newNames : List Var) : Except String (Assignment c) :=
+  let ffMap : ℕ → FF c := fun _id => 0
+  let boolMap : ℕ → Bool := fun _id => false
+  newAssignment' assign orginNames newNames ffMap boolMap
+
+/- Evaluate a term to FF value -/
+def evalTerm {c : ZKConfig} (assign : Assignment c) (t : FFTerm c) : FF c :=
+  match t with
+  | .const v => v
+  | .var v => assign.ff v.id
+  | .add a b => (evalTerm assign a) + (evalTerm assign b)
+  | .sub a b => (evalTerm assign a) - (evalTerm assign b)
+  | .mul a b => (evalTerm assign a) * (evalTerm assign b)
+
+/- Evaluate a formula to a boolean value -/
+def evalFormula {c : ZKConfig}
+   (assign : Assignment c) (f : FFFormula c) (ms : List (FFMacro c)) : Except String Bool := do
+  match f with
+  | .true     => return true
+  | .false    => return false
+  | .bool v   => return assign.bool v.id
+  | .eqZero t => return evalTerm assign t = 0
+  | .and a b  => return (← evalFormula assign a ms) && (← evalFormula assign b ms)
+  | .or a b   => return (← evalFormula assign a ms) || (← evalFormula assign b ms)
+  | .not a    => return !(← evalFormula assign a ms)
+  | .ite c t e => if (← evalFormula assign c ms) then evalFormula assign t ms
+                  else evalFormula assign e ms
+  | .imply a b => return !(← evalFormula assign a ms) || (← evalFormula assign b ms)
+  | .iff a b   => return (← evalFormula assign a ms) == (← evalFormula assign b ms)
+  | .call name args =>
+     match _h_fetchm: fetchMacro ms name with
+     | Except.error e => Except.error e
+     | Except.ok (m,ms') =>
+       let newAssign ← newAssignment assign args m.params
+       evalFormula newAssign m.body ms'
+termination_by (ms.length, sizeOfFormula f)
+decreasing_by
+  any_goals
+    apply Prod.Lex.right
+    simp only [sizeOfFormula]
+    grind
+  -- the call to a macro
+  apply Prod.Lex.left
+  apply fetchMacroLT ms ms' name m _h_fetchm
+
+
+/- Satisfiability of a formula:
+
+   EXIST an assignment σ such that evalFormula return true
+-/
+def isSatFormula {c : ZKConfig} (f : FFFormula c) (ms : List (FFMacro c)) : Prop :=
+  ∃ (σ : Assignment c), evalFormula σ f ms = Except.ok true
+
+
+/- Satisfiability of a system:
+
+  EXIST an assignment σ such that the main formula is true?
+-/
+def isSatSys {c : ZKConfig} (sys : FFConstraintSystem c) : Except String Prop := do
+  let (f,_) ← mainFormula sys
+  return isSatFormula f sys.macros
+
+
+end Llzk.FFConstraints.Satisfiability
