ee8efbad4f1a9e538c988a56245caf007b4484aa
Samir Genaim <genaim@gmail.com>
2026-03-03 07:52:35 +0100

    allow output parameter

--- translator/lean/llzk/unfold.py
+++ translator/lean/llzk/unfold.py
@@ -4,6 +4,7 @@ from pysmt.smtlib.script import smtlibscript_from_formula
 import sys
 
 input_file = sys.argv[1] if len(sys.argv) > 1 else "input.smt2"
+output_file = sys.argv[2] if len(sys.argv) > 2 else "output.smt2"
 
 parser = SmtLibParser()
 
@@ -12,6 +13,6 @@ script = parser.get_script_fname(input_file)
 formula = script.get_last_formula()
 
 # Dump it back to a file
-with open("output.smt2", "w") as f:
+with open(output_file, "w") as f:
 	# This will write out the pure assertions without define-fun
 	smtlibscript_from_formula(formula).serialize(f)
