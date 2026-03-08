d7957097ab355b5c974a96acd9726f04241ab10f
Samir Genaim <genaim@gmail.com>
2026-03-08 21:05:31 +0100

    handling shift by more than k bits

--- translator/lean/llzk/Llzk/SymExec/BitWiseExpr.lean
+++ translator/lean/llzk/Llzk/SymExec/BitWiseExpr.lean
@@ -129,6 +129,38 @@ def sEvalBitWiseNOT {c : ZKConfig}
     newFFVars := newFFVars
   }
 
+def sEvalBitWiseSHLAux {c : ZKConfig}
+  (cfg : SymExecConfig c) (md : CmdMD)
+  (senv : SymEnv c) (v1 : FFTerm c) (v2 : Nat) (outFFVar : FFVar)
+  : Except String (ExprSpec c) := do
+  if v2 >= c.k then
+    -- if shift amount is greater than or equal to bit width, the result is always 0
+    return {
+      inSymEnv := senv,
+      f := FFFormula.eq (FFTerm.var outFFVar) (FFTerm.val 0),
+      resVar := outFFVar,
+      nextId := cfg.nextId
+    }
+  else
+    let bits1Spec := bitify cfg md v1 -- bitify v1
+    let newBits := (bits1Spec.bits.reverse.drop v2).reverse
+    let idxs := List.range c.k
+    let pows := idxs.map (fun i => FFTerm.val (2 ^ (i+v2)))
+    let shiftedBits := List.zipWith (fun bit pow => FFTerm.mul bit pow) newBits pows
+    let sum := match shiftedBits, pows with
+               | b::bs, p::ps => List.foldl (fun acc (bit, pow) => FFTerm.add acc (FFTerm.mul bit pow))
+                        (FFTerm.mul b p)
+                        (List.zip bs ps)
+               | _, _ => FFTerm.val 0 -- they are the same length, we reach this with empty lists
+    let f : FFFormula c := (.and bits1Spec.f (FFFormula.eq (FFTerm.var outFFVar) sum))
+    let newFFVars := bits1Spec.newFFVars
+    return {
+      inSymEnv := senv,
+      f := f,
+      resVar := outFFVar,
+      nextId := bits1Spec.nextId,
+      newFFVars := newFFVars
+    }
 
 def sEvalBitWiseSHLConstShift {c : ZKConfig}
   (cfg : SymExecConfig c) (md : CmdMD)
@@ -136,25 +168,17 @@ def sEvalBitWiseSHLConstShift {c : ZKConfig}
   : Except String (ExprSpec c) := do
   let v1 ← simpleExprToTerm senv s1
   let v2 ← simpleExprToFF senv s2 -- the number of bits
-  let bits1Spec := bitify cfg md v1 -- bitify v1
-  let newBits := (bits1Spec.bits.reverse.drop v2.val).reverse
-  let idxs := List.range c.k
-  let pows := idxs.map (fun i => FFTerm.val (2 ^ (i+v2.val)))
-  let shiftedBits := List.zipWith (fun bit pow => FFTerm.mul bit pow) newBits pows
-  let sum := match shiftedBits, pows with
-             | b::bs, p::ps => List.foldl (fun acc (bit, pow) => FFTerm.add acc (FFTerm.mul bit pow))
-                      (FFTerm.mul b p)
-                      (List.zip bs ps)
-             | _, _ => FFTerm.val 0 -- they are the same length, we reach this with empty lists
-  let f : FFFormula c := (.and bits1Spec.f (FFFormula.eq (FFTerm.var outFFVar) sum))
-  let newFFVars := bits1Spec.newFFVars
-  return {
-     inSymEnv := senv,
-     f := f,
-     resVar := outFFVar,
-     nextId := bits1Spec.nextId,
-     newFFVars := newFFVars
-  }
+  sEvalBitWiseSHLAux cfg md senv v1 v2.val outFFVar
+
+/-
+   outVars1 = shl outVar0 1
+   outVars2 = shl outVar1 2
+   outVars3 = shl outVar2 4
+   outVars4 = shl outVar2 4
+
+
+
+-/
 
 
 def sEvalBitWiseSHL {c : ZKConfig}
@@ -167,31 +191,48 @@ def sEvalBitWiseSHL {c : ZKConfig}
       Except.error "Bitwise shift left with non-constant not implemented yet"
 
 
+
+def sEvalBitWiseSHRAux {c : ZKConfig}
+  (cfg : SymExecConfig c) (md : CmdMD)
+  (senv : SymEnv c) (v1 : FFTerm c) (v2 : Nat) (outFFVar : FFVar)
+  : Except String (ExprSpec c) := do
+  if v2 >= c.k then
+    -- if shift amount is greater than or equal to bit width, the result is always 0
+    return {
+      inSymEnv := senv,
+      f := FFFormula.eq (FFTerm.var outFFVar) (FFTerm.val 0),
+      resVar := outFFVar,
+      nextId := cfg.nextId
+    }
+  else
+    let bits1Spec := bitify cfg md v1 -- bitify v1
+    let newBits := bits1Spec.bits.drop v2
+    let idxs := List.range c.k
+    let pows := idxs.map (fun i => FFTerm.val (2 ^ i))
+    let shiftedBits := List.zipWith (fun bit pow => FFTerm.mul bit pow) newBits pows
+    let sum := match shiftedBits, pows with
+               | b::bs, p::ps => List.foldl (fun acc (bit, pow) => FFTerm.add acc (FFTerm.mul bit pow))
+                        (FFTerm.mul b p)
+                        (List.zip bs ps)
+               | _, _ => FFTerm.val 0 -- they are the same length, we reach this with empty lists
+    let f : FFFormula c := (.and bits1Spec.f (FFFormula.eq (FFTerm.var outFFVar) sum))
+    let newFFVars := bits1Spec.newFFVars
+    return {
+       inSymEnv := senv,
+       f := f,
+       resVar := outFFVar,
+       nextId := bits1Spec.nextId,
+       newFFVars := newFFVars
+    }
+
+
 def sEvalBitWiseSHRConstShift {c : ZKConfig}
   (cfg : SymExecConfig c) (md : CmdMD)
   (senv : SymEnv c) (s1 s2 : SimpleExpr c) (outFFVar : FFVar)
   : Except String (ExprSpec c) := do
   let v1 ← simpleExprToTerm senv s1
   let v2 ← simpleExprToFF senv s2 -- the number of bits
-  let bits1Spec := bitify cfg md v1 -- bitify v1
-  let newBits := bits1Spec.bits.drop v2.val
-  let idxs := List.range c.k
-  let pows := idxs.map (fun i => FFTerm.val (2 ^ i))
-  let shiftedBits := List.zipWith (fun bit pow => FFTerm.mul bit pow) newBits pows
-  let sum := match shiftedBits, pows with
-             | b::bs, p::ps => List.foldl (fun acc (bit, pow) => FFTerm.add acc (FFTerm.mul bit pow))
-                      (FFTerm.mul b p)
-                      (List.zip bs ps)
-             | _, _ => FFTerm.val 0 -- they are the same length, we reach this with empty lists
-  let f : FFFormula c := (.and bits1Spec.f (FFFormula.eq (FFTerm.var outFFVar) sum))
-  let newFFVars := bits1Spec.newFFVars
-  return {
-     inSymEnv := senv,
-     f := f,
-     resVar := outFFVar,
-     nextId := bits1Spec.nextId,
-     newFFVars := newFFVars
-  }
+  sEvalBitWiseSHRAux cfg md senv v1 v2.val outFFVar
 
 def sEvalBitWiseSHR {c : ZKConfig}
   (cfg : SymExecConfig c) (md : CmdMD)
