75106d6fe5b8d697743999b4f711db1cd379d08b
Samir Genaim <genaim@gmail.com>
2026-03-08 11:15:30 +0100

    added config to command line parameters

--- translator/lean/llzk/Main.lean
+++ translator/lean/llzk/Main.lean
@@ -15,40 +15,48 @@ open Llzk.FFConstraints.SMT
 
 open Cli
 
-def symExec (_p : Parsed) (inFile : String) (outStream : IO.FS.Stream) : IO Unit := do
+def symExec (c : ZKConfig) (p : Parsed) (inFile : String) (outStream : IO.FS.Stream) : IO Unit := do
      IO.println s!"Parsing {inFile}..."
      let initialState ← ParserM.fromFile inFile
-     let (prog,_) ← StateT.run (@parseProg F11 []) initialState
+     let (prog,_) ← StateT.run (@parseProg c []) initialState
      IO.println s!"Adding liveness information..."
      let progWithLiveness := addLivenessProg prog
      IO.println s!"Performing symbolic execution..."
-     match @seExecProg F11 {} progWithLiveness "main" with
+     match @seExecProg c {} progWithLiveness "main" with
      | Except.error e =>
          IO.println s!"Error during symbolic execution: {e}"
      | Except.ok constraints =>
        IO.println s!"Generating SMT constraints..."
        IO.println s!""
-       @printConstraintSystem F11 outStream constraints
+       @printConstraintSystem c outStream constraints
        outStream.flush
 
 /- Pretty printing of a given program -/
-def prettyPrinting (p : Parsed) (inFile : String) (outStream : IO.FS.Stream) : IO Unit := do
+def prettyPrinting (c : ZKConfig) (p : Parsed) (inFile : String) (outStream : IO.FS.Stream) : IO Unit := do
      let fc : FormatConfig := {
        indentSize := 2,
        showLiveness := p.hasFlag "showliveness"
      }
      IO.println s!"Parsing {inFile}..."
      let initialState ← ParserM.fromFile inFile
-     let (prog,_) ← StateT.run (@parseProg F11 []) initialState
+     let (prog,_) ← StateT.run (@parseProg c []) initialState
      let progWithLiveness := addLivenessProg prog
      IO.println s!"Pretty printing the input program..."
      IO.println s!""
-     @printProg fc F11 progWithLiveness outStream
+     printProg fc progWithLiveness outStream
      outStream.flush
 
 
+def getZKConfig (p : Parsed) : IO ZKConfig := do
+  let zkConfigStr := p.flag! "zkconfig" |>.as! String
+  match zkConfigStr with
+  | "f11" => return F11
+  | "g64" => return goldilocks64
+  | _ => panic! s!"Unsupported ZKConfig: {zkConfigStr}"
+
 /- Main entry point for the command line interface -/
 def runLlzkCmd (p : Parsed) : IO UInt32 := do
+  let zkConfig ← getZKConfig p
   let input : String := p.positionalArg! "input" |>.as! String
   let outStream ← if p.hasFlag "output" then
     let output : String := p.flag! "output" |>.as! String
@@ -57,9 +65,9 @@ def runLlzkCmd (p : Parsed) : IO UInt32 := do
   else
     IO.getStdout
   if p.hasFlag "prettyprint" then
-     prettyPrinting p input outStream
+     prettyPrinting zkConfig p input outStream
   else if p.hasFlag "symbolicexec" then
-     symExec p input outStream
+     symExec zkConfig p input outStream
   else
      IO.println "No action specified. Use --help for more information."
   return 0
@@ -73,9 +81,12 @@ def llzkCmd : Cmd := `[Cli|
     sl, showliveness;        "Show liveness information for each command."
     pp, prettyprint;         "Parse and pretty-print the input program."
     se, symbolicexec;        "Perform symbolic execution of the input program."
+    zk, zkconfig : String;   "The ZKConfig to use for symbolic execution (f11,g64)"
     o, output : String;      "The output file. If not provided, stdout is used."
   ARGS:
     input : String;      "The input program"
+  EXTENSIONS:
+    defaultValues! #[("zkconfig", "f11")]
 ]
 
 
