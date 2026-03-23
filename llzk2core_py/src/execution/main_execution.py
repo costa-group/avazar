72a6d0a87a746d05a30268a6550070e9d2eae453
tutugordillo <tutugordillo@gmail.com>
2026-03-23 18:25:03 +0100

    creating parser

--- llzk2core_py/src/execution/main_execution.py
+++ llzk2core_py/src/execution/main_execution.py
@@ -1,4 +1,19 @@
+from llzk_dialects.parser import LLZKParser
+from llzk_dialects.felt import FeltDialect
+from llzk_dialects.scf import SCFDialect
+
+def init_llzk_parser(lines:str):
+    p = LLZKParser(lines)
+
+    p.add_dialect(FeltDialect()) 
+    p.add_dialect(SCFDialect())
+    
+    return p 
+
+
 def main(args):
     print("AVAZAR LLZK2CORE Language")
+
+    p = init_llzk_parser()
+    res = p.parse_module()
     
-    pass
