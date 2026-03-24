7f30a473f3b45f90e2ea58bd1761d95688581ea3
Enrique Martín <emartinm@ucm.es>
2026-03-24 18:34:20 +0100

    Sketch IOFormula

--- translator/lean/llzk/Playground/FunctionalFormulas.lean
+++ translator/lean/llzk/Playground/FunctionalFormulas.lean
@@ -30,10 +30,37 @@ If we have two IOFormulas f1 and f2 such that
  f2.auxFFVars ∩ f1.inFFVars = ∅
  f2.auxBoolVars ∩ f1.inBooVars = ∅
 
+Theorem T:
 Any assignment ρ that satisfies f1.f can be extended to an assignment ρ'
 that satisfies f2.f by assigning "arbitrary" values to the variables in
 f2.auxFFVars and f2.auxBoolVars.
 
 
+Other interesting properties about aux variables in IOFormulas (same for bool vars). These should
+follow from the fact that aux variables are "freshly" generated and do not overlap with anything else:
+
+  f1.auxFFVars ∩ f2.auxFFVars = ∅
+  f1.inFFVars ∩ f1.auxFFVars = ∅
+  f2.inFFVars ∩ f2.auxFFVars = ∅
+  f2.inFFVars can contain variables in f1.inFFVars, f1.auxFFVars, or variables that are not in f1 at all.
+
+Proof sketch of Theorem T:
+  Let ρ be an assignment that satisfies f1.f. By definition it assigns values to the variables in
+  f1.inFFVars ⊎ f1.auxFFVars.
+
+  Case distinction on whether f2.inFFVars is a subset of f1.inFFVars ⊎ f1.auxFFVars or not.
+
+  * If f2.inFFVars ⊆ f1.inFFVars ⊎ f1.auxFFVars, then we can directly extend ρ to ρ' by hf, and
+    it will satisfy f2.f.
+
+  * If f2.inFFVars ⊄ f1.inFFVars ⊎ f1.auxFFVars, then f2.inFFVars must contain some variables S that are not
+    in f1.inFFVars ⊎ f1.auxFFVars (i.e., S = f2.inFFVars \ (f1.inFFVars ⊎ f1.auxFFVars)). We can extend ρ
+    with arbitrary values for the variables in S, obtaining ρ''. ρ'' satisfies f1.f and by hf of f2, we can
+    extend it to ρ' that satisfies f2.f.
+
+
+IDEA: when working with assignments, we can find it interesting to keep them minimal, i.e., only assign values
+to the variables that are needed for the next formula. We can always restrict the assignments to subsets of
+relevant variables: ρ|_S
 
 -/
