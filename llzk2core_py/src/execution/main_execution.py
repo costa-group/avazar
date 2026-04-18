424a719c62310f12fa7034a5c0ad6613c6682a92
alexcere <48130030+alexcere@users.noreply.github.com>
2026-04-18 22:55:58 +0200

    Fix args + reading args from file

--- llzk2core_py/src/execution/main_execution.py
+++ llzk2core_py/src/execution/main_execution.py
@@ -1,19 +1,24 @@
+import argparse
+from typing import List
 from llzk_dialects.parser import LLZKParser
 from llzk_dialects.felt import FeltDialect
 from llzk_dialects.scf import SCFDialect
+from llzk_dialects.function import FunctionDialect
+from llzk_dialects.llzk import LLZKDialect
 
-def init_llzk_parser(lines:str):
-    p = LLZKParser(lines)
 
-    p.add_dialect(FeltDialect()) 
-    p.add_dialect(SCFDialect())
-    
+def init_llzk_parser(llzk_plain_str: List[str]):
+    p = LLZKParser(llzk_plain_str)
+
+    # Adding all required dialects
+    p.add_dialects([FeltDialect, SCFDialect, FunctionDialect, LLZKDialect])
+
     return p 
 
 
-def main(args):
-    print("AVAZAR LLZK2CORE Language")
+def main(args: argparse.Namespace):
+    with open(args.source, 'r') as f:
+        llzk_contents = f.readlines()
 
-    p = init_llzk_parser()
+    p = init_llzk_parser(llzk_contents)
     res = p.parse_module()
-    
