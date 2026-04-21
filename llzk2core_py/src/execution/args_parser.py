5ebc1d3af135ecc71dca890e4924d8b1244a6747
alexcere <48130030+alexcere@users.noreply.github.com>
2026-04-21 23:20:06 +0200

    #2 Option for specifying the output file

--- llzk2core_py/src/execution/args_parser.py
+++ llzk2core_py/src/execution/args_parser.py
@@ -2,7 +2,7 @@
 Methods for parsing the options to execute grey
 """
 import argparse
-
+from pathlib import Path
 
 def generate_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description="Avazar Project")
@@ -12,6 +12,12 @@ def generate_parser() -> argparse.ArgumentParser:
     input_options.add_argument("-s", "--source", type=str, help="Local source file name.", required=True,
                                dest="source")
 
+    output_options = parser.add_argument_group("Output Options")
+
+    output_options.add_argument("-o", "--output", type=str, help="Target output file. If not specified, "
+                                                                 "it is stored in the file {source}.core",
+                                dest="target")
+
     #TODO: add option to execute llzk directly instead of reading the file
     
     return parser
@@ -20,5 +26,9 @@ def generate_parser() -> argparse.ArgumentParser:
 def parse_args() -> argparse.Namespace:
     parser = generate_parser()
     parsed_args = parser.parse_args()
+
+    # Generate a default name
+    if parsed_args.target is None:
+        parsed_args.target = Path(Path(parsed_args.source).name).stem + ".core"
 
     return parsed_args
