c2380f479fa9be84f24413dd50d86b87b402b224
alexcere <48130030+alexcere@users.noreply.github.com>
2026-06-11 13:33:02 +0200

    #2 Multidimensional arrays handled lineally

--- translator/llzk2core/tests/test_pod_parse.py
+++ translator/llzk2core/tests/test_pod_parse.py
@@ -11,7 +11,7 @@ class TestPod:
         op = PodNew.parse("%p = pod.new : !pod.type<[@x: !felt.type]>")
         assert op.result == SSAVar("%p")
         assert op.init_records == {}
-        assert op.result_type == Type("!pod.type<[@x: !felt.type]>")
+        assert op.result_type == {"@x": Type("!felt.type")}
 
     def test_new_with_init(self):
         op = PodNew.parse("%p = pod.new {@x = %v0} : !pod.type<[@x: !felt.type]>")
@@ -32,7 +32,16 @@ class TestPod:
         assert op.result == SSAVar("%pod")
         assert "@count" in op.init_records
         assert op.init_records["@count"] == SSAVar("%c2")
-        assert "!pod.type" in op.result_type.name
+        assert "@count" in op.result_type
+        assert "@comp" in op.result_type
+        assert "@params" in op.result_type
+        assert op.result_type["@count"] == Type("index")
+        assert op.result_type["@comp"] == Type("!struct.type<@S<[]>>")
+        assert op.result_type["@params"] == Type("!pod.type<[]>")
+
+    def test_new_empty_pod_type(self):
+        op = PodNew.parse("%p = pod.new : !pod.type<[]>")
+        assert op.result_type == {}
 
     def test_new_match(self):
         assert PodNew.match("%p = pod.new : !pod.type<[]>") is True
@@ -49,11 +58,23 @@ class TestPod:
         assert op.result == SSAVar("%v")
         assert op.pod_ref == SSAVar("%pod")
         assert op.record_name == GlobalVariable("@x")
-        assert len(op.types) == 2
+        assert op.pod_type == {"@x": Type("!felt.type")}
+        assert op.result_type == Type("!felt.type")
 
     def test_read_no_type(self):
         op = PodRead.parse("%v = pod.read %pod [@field]")
-        assert op.types == []
+        assert op.pod_type == {}
+        assert op.result_type is None
+
+    def test_read_complex_pod_type(self):
+        line = ("%v = pod.read %p [@comp] : "
+                "!pod.type<[@count: index, @comp: !struct.type<@S<[]>>, @params: !pod.type<[]>]>, "
+                "!struct.type<@S<[]>>")
+        op = PodRead.parse(line)
+        assert "@count" in op.pod_type
+        assert "@comp" in op.pod_type
+        assert "@params" in op.pod_type
+        assert op.result_type == Type("!struct.type<@S<[]>>")
 
     def test_read_match(self):
         assert PodRead.match("%v = pod.read %p [@x]") is True
@@ -68,10 +89,22 @@ class TestPod:
         assert op.pod_ref == SSAVar("%pod")
         assert op.record_name == GlobalVariable("@x")
         assert op.value == SSAVar("%val")
+        assert op.pod_type == {"@x": Type("!felt.type")}
+        assert op.value_type == Type("!felt.type")
 
     def test_write_no_type(self):
         op = PodWrite.parse("pod.write %pod [@x] = %v")
-        assert op.types == []
+        assert op.pod_type == {}
+        assert op.value_type is None
+
+    def test_write_complex_pod_type(self):
+        line = ("pod.write %p [@count] = %c : "
+                "!pod.type<[@count: index, @comp: !struct.type<@S<[]>>, @params: !pod.type<[]>]>, "
+                "index")
+        op = PodWrite.parse(line)
+        assert "@count" in op.pod_type
+        assert "@comp" in op.pod_type
+        assert op.value_type == Type("index")
 
     def test_write_match(self):
         assert PodWrite.match("pod.write %p [@x] = %v") is True
