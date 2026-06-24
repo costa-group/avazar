8cac502cfa3fca6a19d0b2dd583cffffee05953b
tutugordillo <tutugordillo@gmail.com>
2026-06-24 14:39:24 +0200

    working on scripting

--- avazar.py
+++ avazar.py
@@ -30,46 +30,35 @@ def main():
     parser.add_argument("-s", "--source", type=str, required=True,
                         help="Circom circuit file."  )
     
-    # Flag -llzk (mandatory)
-    parser.add_argument("-llzk", type=str, required=True, help="LLZK file corresponding to the circuit specified in -s"  )
+    # # Flag -llzk (mandatory)
+    # parser.add_argument("-llzk", type=str, required=True, help="LLZK file corresponding to the circuit specified in -s"  )
     
     # Flag -out (mandatory)
     parser.add_argument( "-out", type=str, required=True, help="Output Path"  )
+
+    #Flag -solver (default ffsol)
+    parser.add_argument( "-solver", type=str, required=False, default="ffsol", help = "Solver to be used")
     
-    # Parsear los argumentos de la línea de comandos
-    args: argparse.Namespace = parser.parse_args()
+    args = parser.parse_args()
     
     try:
-        # 2. Validación de existencia de los ficheros de lectura
         if not os.path.isfile(args.source):
-            raise FileNotFoundError(f"El fichero source especificado no existe: {args.source}")
+            raise FileNotFoundError(f"Source file {args.source} does not exist")
             
-        if not os.path.isfile(args.llzk):
-            raise FileNotFoundError(f"El fichero llzk especificado no existe: {args.llzk}")
+        # if not os.path.isfile(args.llzk):
+        #     raise FileNotFoundError(f"El fichero llzk especificado no existe: {args.llzk}")
+                
+        if args.out and not os.path.exists(args.out):
+            logging.info(f"Dir {args.out} does not exist. Creating...")
+            os.makedirs(args.out, exist_ok=True)
         
-        # 3. Obtener el directorio del tercer fichero (-out)
-        # os.path.dirname("ruta/al/fichero.txt") -> devuelve "ruta/al"
-        # os.path.abspath asegura que la ruta sea absoluta y limpia
-        ruta_absoluta_out: str = os.path.abspath(args.out)
-        directorio_salida: str = os.path.dirname(ruta_absoluta_out)
-        
-        # Opcional: Crear el directorio de salida si no existe
-        if directorio_salida and not os.path.exists(directorio_salida):
-            logging.info(f"El directorio {directorio_salida} no existe. Creándolo...")
-            os.makedirs(directorio_salida, exist_ok=True)
-
-        # 4. Ejecutar la lógica principal
-        procesar_ficheros(
-            fichero_source=args.source, 
-            fichero_llzk=args.llzk, 
-            directorio_out=directorio_salida
-        )
-        
+            
+            
     except FileNotFoundError as e:
-        logging.error(f"Error de archivo: {e}")
+        logging.error(f"File error: {e}")
         sys.exit(1)
     except Exception as e:
-        logging.error(f"Ocurrió un error inesperado: {e}")
+        logging.error(f"Unexpeted error: {e}")
         sys.exit(1)
     
 if __name__ == '__main__':
